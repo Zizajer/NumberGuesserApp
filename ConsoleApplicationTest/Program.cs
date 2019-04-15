a9ba3ad44c784de16ab112d6752db08bb5841f63
Piotr <zizi5673@gmail.com>
2019-04-15 21:39:16 +0200

    Added posibility for user begin again the game.

--- ConsoleApplicationTest/Program.cs
+++ ConsoleApplicationTest/Program.cs
@@ -13,15 +13,18 @@ namespace NumberGusserApp
             Random random = new Random();
             int randomNumber = random.Next(1, 10);
             int guessedNumber = 0;
+            String input = null;
 
-            bool isGuessed = false;
+            bool hasDecided = false;
+            bool isTrying = true;
 
             printInColor(ConsoleColor.Green,"Hello in Guess The Number Game !!!");
             
-            while (!isGuessed)
+            while (isTrying)
             {
+                hasDecided = false;
                 Console.WriteLine("Please write a number from 1 to 10 : ");
-                String input = Console.ReadLine();
+                input = Console.ReadLine();
                 try
                 {
                     guessedNumber = Int32.Parse(input);
@@ -30,14 +33,35 @@ namespace NumberGusserApp
                 {
                     Console.WriteLine("Unable to parse '{input}' ");
                 }
-                
-                if (guessedNumber == randomNumber)
-                    isGuessed = true;
-                else
-                    printInColor(ConsoleColor.Red,"Try again.");
-            }
 
-            printInColor(ConsoleColor.Yellow, "Congratulations you guessed it.");
+                if (guessedNumber == randomNumber)
+                {
+                    printInColor(ConsoleColor.Yellow, "Congratulations you guessed it.");
+                    while (!hasDecided)
+                    {
+                        printInColor(ConsoleColor.Yellow, "Do you want to try again ? (Y/N) ");
+                        input = Console.ReadLine();
+                        if (input.Equals("N"))
+                        {
+                            isTrying = false;
+                            hasDecided = true;
+                        }
+                        else if (input.Equals("Y"))
+                        {
+                            randomNumber = random.Next(1, 10);
+                            isTrying = true;
+                            hasDecided = true;
+                        }
+                        else
+                        {
+                            hasDecided = false;
+                        }
+                    }
+                }
+                else
+                    printInColor(ConsoleColor.Red, "Try again.");
+            }
+            
 
             Console.WriteLine("Please press any key to end the game.");
             Console.ReadKey();
